37eef4251b7f0c7b79978b110761134b36ef9608
Ece Yurtseven <eceeyurtseven@gmail.com>
2024-08-08 02:30:30 +0300

    changes after the 4th video

---
+++ client.py
@@ -0,0 +1,58 @@
+import flwr as fl
+import torch
+from collections import OrderedDict
+from typing import Dict, Tuple
+from flwr.common import Scalar, NDArrays
+
+from model import Net, train, test
+
+class FlowerClient(fl.client.NumPyClient):
+    def __init__(self, trainloader, valloader, num_classes) -> None:
+        super().__init__()
+
+        self.trainloader = trainloader
+        self.valloader = valloader
+
+        self.model = Net(num_classes)
+
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        def set_parameters(self, parameters):
+            params_dict = zip(self.model.state_dict().keys, parameters)
+            
+            state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
+
+            self.model.load_state_dict(state_dict, strict=True)
+
+        def get_parameters(self, config: Dict[str, Scalar]):
+            return[val.cpu().numpy() for _, val in self.model.state_dict().items()]
+
+        def fit(self, parameters, config):
+            self.set_parameters(parameters)
+
+            lr = config["lr"]
+            momentum = config["momentum"]
+            epochs = config["local_epochs"]
+
+            optim = torch.optim.SGD(self.model.parameters(), lr=lr, momentum=momentum)
+
+            #local training
+            train(self.model, self.trainloader, optim, 1, self.device)
+
+            return self.get_parameters(), len(self.trainloader), {}
+        
+    def evaluate(self, parameters: NDArrays, config: Dict[str, Scalar]):
+        self.set_parameters(parameters)
+
+        loss, accuracy = test(self.model, self.valloader, self.device)
+
+        return float(loss), len(self.valloader), {"accuracy": accuracy}
+
+def generate_client_fn(trainloaders, valloaders, num_classes):
+
+    def client_fn(cid: str):
+
+        FlowerClient(trainloader=trainloaders[int(cid)], valloader=valloaders[int(cid)], num_classes=num_classes)
+
+
+    return client_fn
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import hydra
 from omegaconf import DictConfig, OmegaConf
 
+from client import generate_client_fn
 from dataset import prepare_dataset
 
 @hydra.main(config_path="conf", config_name="base", version_base=None)
@@ -11,6 +12,8 @@ def main(cfg: DictConfig):
     trainloaders, validationloaders, testloader = prepare_dataset(cfg.num_clients, cfg.batch_size)
 
     print(len(trainloaders), len(trainloaders[0].dataset))
+
+    client_fn = generate_client_fn(trainloaders, validationloaders, cfg.num_classes)
 
 
 if __name__ == "__main__":
