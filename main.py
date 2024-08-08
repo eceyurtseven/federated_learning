dedf347765b9c005072de2a7c25b90387567f5ef
Ece Yurtseven <eceeyurtseven@gmail.com>
2024-08-08 10:44:29 +0300

    changes after the 5th video

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import os
 import hydra
 from omegaconf import DictConfig, OmegaConf
 
+import flwr as fl
+
 from client import generate_client_fn
 from dataset import prepare_dataset
+from server import get_on_fit_config, get_evaluate_fn
 
 @hydra.main(config_path="conf", config_name="base", version_base=None)
 def main(cfg: DictConfig):
@@ -14,6 +17,9 @@ def main(cfg: DictConfig):
     print(len(trainloaders), len(trainloaders[0].dataset))
 
     client_fn = generate_client_fn(trainloaders, validationloaders, cfg.num_classes)
+
+    strategy = fl.server.strategy.FedAvg(fraction_fit=0.00001, min_fit_clients=cfg.num_clients_per_round_fit, fraction_evaluate=0.00001, min_evaluate_clients=cfg.num_clients_per_round_eval, min_available_clients=cfg.num_clients, on_fit_config_fn=get_on_fit_config(cfg.config_fit), evaluate_fn=get_evaluate_fn(cfg.num_classes, testloader))  
+
 
 
 if __name__ == "__main__":
